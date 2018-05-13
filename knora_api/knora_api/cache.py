a2cef1def936b6a881c077818d8f64a32f38b914
Sacha Kaufmann <sacha@kaufmann.lu>
2018-05-13 17:10:57 +0200

    provide a checksum in cache

--- knora_api/knora_api/cache.py
+++ knora_api/knora_api/cache.py
@@ -64,7 +64,7 @@ class Cache(object):
         try:
             cur = self._connection.cursor()
             cur.execute("DROP TABLE IF EXISTS Cache;")
-            cur.execute("CREATE TABLE Cache(key text PRIMARY KEY , value text NOT NULL);")
+            cur.execute("CREATE TABLE Cache(key text PRIMARY KEY , value text NOT NULL, checksum text NULL);")
             cur.close()
             self._connection.commit()
             return True
@@ -82,6 +82,22 @@ class Cache(object):
         """
         try:
             cur = self._connection.cursor()
+            cur.execute("SELECT value, checksum FROM Cache WHERE key=:key;", {'key': key})
+            record = cur.fetchone()
+            return record[0] if record else None
+        except Exception as e:
+            print(e)
+        return None
+
+    def get_value(self, key):
+        """
+        gets the key's value
+
+        :param key:
+        :return:
+        """
+        try:
+            cur = self._connection.cursor()
             cur.execute("SELECT value FROM Cache WHERE key=:key;", {'key': key})
             record = cur.fetchone()
             return record[0] if record else None
@@ -89,7 +105,7 @@ class Cache(object):
             print(e)
         return None
 
-    def set(self, key, value):
+    def set(self, key, value, checksum=None):
         """
         set a key and its value
 
@@ -99,16 +115,16 @@ class Cache(object):
         """
         try:
             cur = self._connection.cursor()
-            cur.execute("INSERT INTO Cache VALUES (:key, :value);", {'key': key, 'value': value})
+            cur.execute("INSERT INTO Cache VALUES (:key, :value, :checksum);", {'key': key, 'value': value, 'checksum': checksum})
             self._connection.commit()
             return True
         except Exception as e:
             print(e)
         return False
 
-    def setnx(self, key, value):
+    def setnx(self, key, value, checksum=None):
         """
-        sets a key and it value, only if the key does not exists
+        sets a key and its value, only if the key does not exists
         :param key:
         :param value:
         :return:
@@ -116,7 +132,7 @@ class Cache(object):
 
         if self.get(key):
             return False
-        return self.set(key, value)
+        return self.set(key, value, checksum)
 
     @property
     def cache_dir(self):
